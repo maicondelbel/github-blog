281fcac1fab23446312d0fd750555f0db8c6fce5
Maicon P. Del Bel <maicon.delbel@gmail.com>
2022-10-13 18:28:11 -0300

    Deploying in GH Pages

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const HeaderContainer = styled.header`
   height: 18.5rem;
   background-color: ${(props) => props.theme['base-profile']};
-  background-image: url(/header-background.png);
+  background-image: url(header-background.png);
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 50%, 50%;
