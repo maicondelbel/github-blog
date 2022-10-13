ce5dcba3897e5cc16008723f610b2edc121d9849
Maicon P. Del Bel <maicon.delbel@gmail.com>
2022-10-13 18:20:29 -0300

    Deploying in GH Pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,9 +6,18 @@ import { Post } from './pages/Post'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/post/:slug" element={<Post />}></Route>
+      <Route
+        path={`${import.meta.env.VITE_BASE_URL}/`}
+        element={<DefaultLayout />}
+      >
+        <Route
+          path={`${import.meta.env.VITE_BASE_URL}/`}
+          element={<Home />}
+        ></Route>
+        <Route
+          path={`${import.meta.env.VITE_BASE_URL}/post/:slug`}
+          element={<Post />}
+        ></Route>
       </Route>
     </Routes>
   )
