ceeacdf2e5ded8577267b099b1a846e827932367
Maicon P. Del Bel <maicon.delbel@gmail.com>
2022-11-16 10:33:29 -0300

    Updates

--- assets/index.c4de7bf1.js
+++ assets/index.c4de7bf1.js
@@ -1153,4 +1153,4 @@ p, span {
 :focus {
 	outline: 0;
 }
-`,C_={blue:"#3294F8","base-title":"#E7EDF4","base-subtitle":"#C4D4E3","base-text":"#AFC2D4","base-span":"#7B96B2","base-label":"#3A536B","base-border":"#1C2F41","base-post":"#112131","base-profile":"#0B1B2B","base-background":"#071422","base-input":"#040F1A"};function E_(){return Pe(f7,{theme:C_,children:[J(m4,{basename:"",children:J(y8,{children:J(b_,{})})}),J(S_,{})]})}Ac.createRoot(document.getElementById("root")).render(J(Xe.StrictMode,{children:J(E_,{})}));
+`,C_={blue:"#3294F8","base-title":"#E7EDF4","base-subtitle":"#C4D4E3","base-text":"#AFC2D4","base-span":"#7B96B2","base-label":"#3A536B","base-border":"#1C2F41","base-post":"#112131","base-profile":"#0B1B2B","base-background":"#071422","base-input":"#040F1A"};function E_(){return Pe(f7,{theme:C_,children:[J(m4,{basename:"/github-blog/",children:J(y8,{children:J(b_,{})})}),J(S_,{})]})}Ac.createRoot(document.getElementById("root")).render(J(Xe.StrictMode,{children:J(E_,{})}));
